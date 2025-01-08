dbfb71e437db67a2172c37f84bf625e597782998
D00Dth <d00dthgaming@gmail.com>
2025-01-08 22:28:23 +0100

    ajout du menu de selection du musee par pays et changement de scene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -14,9 +14,48 @@ public class MenuManager : MonoBehaviour
     private bool isDefaultSize = true;
 
 
+    [Header("France")]
     [SerializeField] private Button franceButton;
+    [SerializeField] private Sprite franceMuseumSprite;
+
+
+    [Header("England")]
     [SerializeField] private Button englandButton;
+    [SerializeField] private Sprite englandMuseumSprite;
+
+
+    [Header("Germany")]
     [SerializeField] private Button germanyButton;
+    [SerializeField] private Sprite germanyMuseumSprite;
+
+    private Dictionary<Button, MuseumData> buttonToSpriteMap;
+    [SerializeField] private Image museumImg;
+    [SerializeField] private Button clickedButton;
+    [SerializeField] private Button validateButton;
+
+    public struct MuseumData
+    {
+        public Sprite museumSprite;
+        public string museumName;
+
+        public MuseumData(Sprite sprite, string name)
+        {
+            museumSprite = sprite;
+            museumName = name;
+        }
+    }
+
+
+
+    void Start()
+    {
+        buttonToSpriteMap = new Dictionary<Button, MuseumData>
+        {
+            { franceButton, new MuseumData(franceMuseumSprite, "France") },
+            { englandButton, new MuseumData(englandMuseumSprite, "England") },
+            { germanyButton, new MuseumData(germanyMuseumSprite, "Germany") }
+        };
+    }
 
 
     public void ToggleUEMenu()
@@ -27,25 +66,57 @@ public class MenuManager : MonoBehaviour
 
     public void ToggleEUMuseumButton(Button buttonClicked)
     {
+        clickedButton = isDefaultSize ? buttonClicked : null;
+
         GridLayoutGroup gridLayout = museumButtonContainer.GetComponent<GridLayoutGroup>();
 
         Vector2 largeSize = new Vector2(500, 50);
-        Vector2 smallSize = new Vector2(150, 50);
+        Vector2 smallSize = new Vector2(200, 50);
 
         Vector2 sizeButton = isDefaultSize ? smallSize : largeSize;
 
-        LeanTween.value(gameObject, gridLayout.cellSize, sizeButton, 1f)
+        LeanTween.value(gameObject, gridLayout.cellSize, sizeButton, 1.0f)
             .setOnUpdate((Vector2 size) =>
             {
                 gridLayout.cellSize = size;
             })
             .setEase(LeanTweenType.easeInOutSine);
 
-        if(isDefaultSize) DisableButtonInteraction(buttonClicked);
-        else ResetButtonInteraction();
+        if(isDefaultSize) 
+        {
+            Sprite museumSelectedSprite = GetSpriteForButton(buttonClicked);
+            StartCoroutine(ShowMuseumSelected(museumSelectedSprite));
+            DisableButtonInteraction(buttonClicked);
+        }
+        else 
+        {
+            HideMuseumSelected();
+            ResetButtonInteraction();
+        }
 
         isDefaultSize = !isDefaultSize;
     }
+
+    private Sprite GetSpriteForButton(Button buttonClicked)
+    {
+        if (buttonToSpriteMap.TryGetValue(buttonClicked, out MuseumData museumData)) return museumData.museumSprite;
+        else return null;
+    }
+
+    public IEnumerator ShowMuseumSelected(Sprite museumSelectedSprite)
+    {
+        yield return new WaitForSeconds(1.0f);
+        museumImg.gameObject.SetActive(true);
+        validateButton.gameObject.SetActive(true);
+        museumImg.sprite = museumSelectedSprite;
+    }
+
+    public void HideMuseumSelected()
+    {
+        validateButton.gameObject.SetActive(false);
+        museumImg.gameObject.SetActive(false);
+    }
+
 
     #region BUTTON EU MUSEUM
     public void DisableButtonInteraction(Button buttonClicked)
@@ -61,6 +132,14 @@ public class MenuManager : MonoBehaviour
         englandButton.interactable = true; 
         germanyButton.interactable = true; 
     }
-
     #endregion
+
+    public void LoadScene()
+    {
+        if(buttonToSpriteMap.TryGetValue(clickedButton, out MuseumData museumData))
+        {
+            SceneManager.LoadScene(museumData.museumName);
+        }
+    }
+
 }
